2f156180fa91ec35735e36527624b14f642d6cf8
Alex Wong <alexwongsenhung@gmail.com>
2018-06-24 14:18:22 -0700

    add heroku backend server url

--- src/server/index.js
+++ src/server/index.js
@@ -4,4 +4,6 @@ import VueResource from 'vue-resource';
 Vue.use(VueResource);
 
 /* the backend address */
-Vue.http.options.root = 'http://localhost:8000'; /* local dev */
+Vue.http.options.root = 
+    // 'http://localhost:8000'; /* local dev */
+    'https://treehole-server.herokuapp.com/'; /* heroku server */
