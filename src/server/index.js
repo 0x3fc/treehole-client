5f75b1f39c7da03862d703e74744346189c6c19e
Alex Wong <alexwongsenhung@gmail.com>
2018-06-24 14:56:13 -0700

    clean up

--- src/server/index.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-import VueResource from 'vue-resource';
-
-Vue.use(VueResource);
-
-/* the backend address */
-Vue.http.options.root = 
-    // 'http://localhost:8000'; /* local dev */
-    'https://treehole-server.herokuapp.com/'; /* heroku server */
